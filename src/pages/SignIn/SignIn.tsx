b334268de459690afc3085204e7e7e70d28b52ed
MiVladie <justvladon@outlook.com>
2021-04-03 12:07:39 -0500

    🥑 refactured sign in/up code

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,83 +1,22 @@
 import React, { useState } from 'react';
 
-import { Link } from 'react-router-dom';
+import Auth from 'containers/Auth/Auth';
 
-import Container from 'hoc/Container/Container';
-import Input from 'components/ui/Input/Input';
-import Button from 'components/ui/Button/Button';
-
-import classes from './SignIn.module.scss';
+interface Credentials {
+	email: string;
+	password: string;
+}
 
 const SignIn: React.FC = () => {
-	const [email, setEmail] = useState<string>('');
-	const [password, setPassword] = useState<string>('');
+	const [loading, setLoading] = useState<boolean>(false);
 
-	const onSubmitHandler = async () => {
+	const submitHandler = ({ email, password }: Credentials) => {
+		setLoading(true);
+
 		console.log({ email, password });
-
-		return;
 	};
 
-	return (
-		<div className={classes.SignIn}>
-			<Container className={classes.Actions}>
-				<div className={classes.Wrapper}>
-					<div className={classes.Title}>
-						<h1 className={classes.Heading}>Sign In</h1>
-
-						<div className={classes.Line} />
-					</div>
-
-					<form className={classes.Form}>
-						<Input name='email' type='email' placeholder='Your email..' onChange={setEmail} value={email} />
-						<Input
-							name='password'
-							type='password'
-							placeholder='Your password..'
-							onChange={setPassword}
-							value={password}
-						/>
-					</form>
-
-					<Button className={classes.Submit} onClick={onSubmitHandler}>
-						Login
-					</Button>
-
-					<Link className={classes.Redirect} to='/sign-up'>
-						I am a new user
-					</Link>
-				</div>
-			</Container>
-
-			<div className={classes.Info}>
-				<Container>
-					<div className={classes.Wrapper}>
-						<div className={classes.Title}>
-							<h1 className={classes.Heading}>Bellespace</h1>
-
-							<div className={classes.Line} />
-						</div>
-
-						<p className={classes.Text}>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam feugiat lorem auctor,
-							pretium dolor eu, convallis tortor. Vestibulum mattis condimentum lectus vel pretium. Morbi
-							eget accumsan ante. Nullam ut varius risus. Phasellus quam nisl, sollicitudin sit amet
-							scelerisque at, facilisis nec dolor. Integer porttitor venenatis dolor id malesuada.
-						</p>
-
-						<p className={classes.Extra}>
-							Powered by{' '}
-							<a href='https://letscomit.com/' target='__blank'>
-								letscomit
-							</a>
-						</p>
-					</div>
-				</Container>
-
-				<div className={classes.Circle} />
-			</div>
-		</div>
-	);
+	return <Auth action='login' onSubmit={submitHandler} loading={loading} />;
 };
 
 export default SignIn;
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,106 +1,24 @@
 import React, { useState } from 'react';
 
-import { Link } from 'react-router-dom';
+import Auth from 'containers/Auth/Auth';
 
-import Container from 'hoc/Container/Container';
-import Input from 'components/ui/Input/Input';
-import Button from 'components/ui/Button/Button';
-
-import classes from './SignUp.module.scss';
-import Checkbox from 'components/ui/Checkbox/Checkbox';
+interface Credentials {
+	email: string;
+	password: string;
+	confirmPassword: string;
+	agree: boolean;
+}
 
 const SignUp: React.FC = () => {
-	const [email, setEmail] = useState<string>('');
-	const [password, setPassword] = useState<string>('');
-	const [confirmPassword, setConfirmPassword] = useState<string>('');
-	const [agree, setAgree] = useState<boolean>(false);
+	const [loading, setLoading] = useState<boolean>(false);
 
-	const onSubmitHandler = async () => {
+	const submitHandler = ({ email, password, confirmPassword, agree }: Credentials) => {
+		setLoading(true);
+
 		console.log({ email, password, confirmPassword, agree });
-
-		return;
 	};
 
-	return (
-		<div className={classes.SignIn}>
-			<Container className={classes.Actions}>
-				<div className={classes.Wrapper}>
-					<div className={classes.Title}>
-						<h1 className={classes.Heading}>Sign Up</h1>
-
-						<div className={classes.Line} />
-					</div>
-
-					<form className={classes.Form}>
-						<Input name='email' type='email' placeholder='Your email..' onChange={setEmail} value={email} />
-						<Input
-							name='password'
-							type='password'
-							placeholder='Your password..'
-							onChange={setPassword}
-							value={password}
-						/>
-						<Input
-							name='confirmPassword'
-							type='password'
-							placeholder='Confirm password..'
-							onChange={setConfirmPassword}
-							value={confirmPassword}
-						/>
-					</form>
-
-					<Checkbox
-						className={classes.Legal}
-						name='agree'
-						onChange={setAgree}
-						value={agree}
-						label={
-							<span>
-								I agree to the <Link to='/terms-and-conditions'>Terms & Conditions</Link> and{' '}
-								<Link to='/privacy-policy'>Privacy Policy</Link>
-							</span>
-						}
-					/>
-
-					<Button className={classes.Submit} onClick={onSubmitHandler}>
-						Register
-					</Button>
-
-					<Link className={classes.Redirect} to='/sign-in'>
-						I already have an account
-					</Link>
-				</div>
-			</Container>
-
-			<div className={classes.Info}>
-				<Container>
-					<div className={classes.Wrapper}>
-						<div className={classes.Title}>
-							<h1 className={classes.Heading}>Bellespace</h1>
-
-							<div className={classes.Line} />
-						</div>
-
-						<p className={classes.Text}>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam feugiat lorem auctor,
-							pretium dolor eu, convallis tortor. Vestibulum mattis condimentum lectus vel pretium. Morbi
-							eget accumsan ante. Nullam ut varius risus. Phasellus quam nisl, sollicitudin sit amet
-							scelerisque at, facilisis nec dolor. Integer porttitor venenatis dolor id malesuada.
-						</p>
-
-						<p className={classes.Extra}>
-							Powered by{' '}
-							<a href='https://letscomit.com/' target='__blank'>
-								letscomit
-							</a>
-						</p>
-					</div>
-				</Container>
-
-				<div className={classes.Circle} />
-			</div>
-		</div>
-	);
+	return <Auth action='register' onSubmit={submitHandler} loading={loading} />;
 };
 
 export default SignUp;
