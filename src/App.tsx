45b0fe0b5e4001b601b731c99dbb6001a64f0f89
MiVladie <justvladon@outlook.com>
2021-04-04 14:05:40 -0500

    🎃 fixed routing for gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Layout from 'hoc/Layout/Layout';
 
 import SignIn from 'pages/SignIn/SignIn';
 import SignUp from 'pages/SignUp/SignUp';
 import Projects from 'pages/Projects/Projects';
+import Project from 'pages/Project/Project';
 
 const App: React.FC = () => {
 	return (
@@ -14,9 +15,11 @@ const App: React.FC = () => {
 			<Route
 				render={({ location }) => (
 					<Switch location={location}>
+						<Redirect exact path='/' to='/sign-in' />
 						<Route path='/sign-in' exact component={SignIn} />
 						<Route path='/sign-up' exact component={SignUp} />
 						<Route path='/projects' exact component={Projects} />
+						<Route path='/projects/:id' exact component={Project} />
 					</Switch>
 				)}
 			/>
