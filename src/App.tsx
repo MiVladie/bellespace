d5069a09548b2ef4942e94df669283d46024155c
MiVladie <justvladon@outlook.com>
2021-04-10 09:52:45 -0500

    🦄 initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,6 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Layout from 'hoc/Layout/Layout';
 
 import SignIn from 'pages/SignIn/SignIn';
-import SignUp from 'pages/SignUp/SignUp';
-import Projects from 'pages/Projects/Projects';
-import Project from 'pages/Project/Project';
 
 const App: React.FC = () => {
 	return (
@@ -17,9 +14,6 @@ const App: React.FC = () => {
 					<Switch location={location}>
 						<Redirect exact path='/' to='/sign-in' />
 						<Route path='/sign-in' exact component={SignIn} />
-						<Route path='/sign-up' exact component={SignUp} />
-						<Route path='/projects' exact component={Projects} />
-						<Route path='/projects/:id' exact component={Project} />
 					</Switch>
 				)}
 			/>
--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,30 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { useHistory } from 'react-router-dom';
-
-import Auth from 'containers/Auth/Auth';
-
-interface Credentials {
-	email: string;
-	password: string;
-}
+import classes from './SignIn.module.scss';
 
 const SignIn: React.FC = () => {
-	const [loading, setLoading] = useState<boolean>(false);
-
-	const history = useHistory();
-
-	const submitHandler = async ({ email, password }: Credentials) => {
-		setLoading(true);
-
-		console.log({ email, password });
-
-		await new Promise((resolve) => setTimeout(resolve, 1500));
-
-		history.push('/projects');
-	};
-
-	return <Auth action='login' onSubmit={submitHandler} loading={loading} />;
+	return <div className={classes.SignIn}>SignIn</div>;
 };
 
 export default SignIn;
