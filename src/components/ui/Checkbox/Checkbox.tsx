d067afdc917b53fa21d1decf972519c57c26f980
MiVladie <justvladon@outlook.com>
2021-04-11 21:00:42 -0500

    ⛲ added Checkbox error message

--- src/components/ui/Checkbox/Checkbox.tsx
+++ src/components/ui/Checkbox/Checkbox.tsx
@@ -7,25 +7,35 @@ interface Props {
 	name: string;
 	value: boolean;
 	onChange: (e: boolean) => void;
+	onFocus?: () => void;
+	error?: string | null;
 	label?: string | React.ReactNode;
 	dark?: boolean;
 	disabled?: boolean;
 }
 
-const Checkbox: React.FC<Props> = ({ className, name, value, onChange, label, dark, disabled }) => {
+const Checkbox: React.FC<Props> = ({ className, name, value, onChange, onFocus, error, label, dark, disabled }) => {
 	return (
-		<div className={[classes.Wrapper, dark ? classes.Dark : '', className].join(' ')}>
-			<input
-				id={name}
-				className={classes.Checkbox}
-				type='checkbox'
-				value={value ? 1 : 0}
-				onChange={(e) => !disabled && onChange(e.target.checked)}
-				disabled={disabled}
-			/>
+		<>
+			<div
+				className={[classes.Wrapper, dark ? classes.Dark : null, error ? classes.Error : null, className].join(
+					' '
+				)}>
+				<input
+					id={name}
+					className={classes.Checkbox}
+					type='checkbox'
+					value={value ? 1 : 0}
+					onChange={(e) => !disabled && onChange(e.target.checked)}
+					onFocus={onFocus}
+					disabled={disabled}
+				/>
 
-			<label htmlFor={name}>{label}</label>
-		</div>
+				<label htmlFor={name}>{label}</label>
+
+				{error && <small className={classes.Message}>{error}</small>}
+			</div>
+		</>
 	);
 };
 
