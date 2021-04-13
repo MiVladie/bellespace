05726bec97c36ef4ad46aa7ec5363d6adcd2b759
MiVladie <justvladon@outlook.com>
2021-04-12 21:00:56 -0500

    🍭 added Loader container

---
+++ src/containers/Loader/Loader.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Spinner from 'components/ui/Spinner/Spinner';
+
+import classes from './Loader.module.scss';
+
+interface Props {
+	className?: string;
+	style?: object;
+	dark?: boolean;
+	size?: number;
+}
+
+const Loader: React.FC<Props> = ({ className, style, dark, size = 64 }) => {
+	return (
+		<div className={[classes.Loader, dark ? classes.Dark : null, className].join(' ')} style={style}>
+			<Spinner size={size} dark={!dark} />
+
+			<h1 className={classes.Heading}>Loading..</h1>
+		</div>
+	);
+};
+
+export default Loader;
