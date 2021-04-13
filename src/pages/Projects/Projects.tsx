35648ef2f4b13dfb4bb5442c29c05033d36c9a63
MiVladie <justvladon@outlook.com>
2021-04-13 10:20:58 -0500

    🦥 added Projects page

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -3,7 +3,29 @@ import React from 'react';
 import classes from './Projects.module.scss';
 
 const Projects: React.FC = () => {
-	return <div className={classes.Projects}>Projects</div>;
+	return (
+		<div className={classes.Projects}>
+			<div className={classes.Selection}>
+				<div className={classes.Wrapper}>
+					<h1 className={classes.Heading}>Select Your Project..</h1>
+					<div className={classes.Line} />
+
+					<div className={classes.Gallery}></div>
+				</div>
+
+				<div className={classes.Circle} />
+			</div>
+
+			<div className={classes.Library}>
+				<div className={classes.Wrapper}>
+					<h1 className={classes.Heading}>Or Explore Existing..</h1>
+					<div className={classes.Line} />
+
+					<div className={classes.Gallery}></div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Projects;
