59645d98310d79f3022db656c38eb13b59dc4722
MiVladie <justvladon@outlook.com>
2021-04-16 19:58:34 -0500

    🇦🇨 added Sidebar component

---
+++ src/hoc/Sidebar/Sidebar.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+
+import classes from './Sidebar.module.scss';
+
+interface Props {
+	className?: string;
+	visible?: boolean;
+	reverse?: boolean;
+	children: React.ReactNode;
+}
+
+const Sidebar: React.FC<Props> = ({ className, visible = true, reverse, children }) => {
+	const [width, setWidth] = useState<number>(window.innerWidth);
+	const [snapped, setSnapped] = useState<boolean>(false);
+
+	useEffect(() => {
+		window.addEventListener('resize', () => setWidth(window.innerWidth));
+
+		return () => window.removeEventListener('resize', () => setWidth(window.innerWidth));
+	}, []);
+
+	useEffect(() => {
+		if (!visible) {
+			setSnapped(false);
+		}
+	}, [visible]);
+
+	useEffect(() => {
+		if (width < 720 && snapped) {
+			setSnapped(false);
+		}
+	}, [width, snapped]);
+
+	const snapClickHandler = () => {
+		if (width >= 720) {
+			setSnapped((prevState) => !prevState);
+		}
+	};
+
+	if (!visible) {
+		return null;
+	}
+
+	return (
+		<div
+			className={[
+				classes.Sidebar,
+				snapped ? classes.Snapped : null,
+				reverse ? classes.Reverse : null,
+				className
+			].join(' ')}>
+			{children}
+
+			<div className={classes.Toggle} onClick={snapClickHandler}>
+				<span className={classes.Hook} />
+			</div>
+		</div>
+	);
+};
+
+export default Sidebar;
