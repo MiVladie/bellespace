07a3a7e25bdadb30fdf07a5dc23e3be41135522c
MiVladie <justvladon@outlook.com>
2021-04-23 00:15:23 -0500

    ⛲ refractured Input component

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 import { IField } from 'interfaces';
 import { HelpOutline } from '@material-ui/icons';
 
-import classes from './Input.module.scss';
 import Tooltip from '../Tooltip/Tooltip';
+
+import classes from './Input.module.scss';
 
 interface Props extends IField {
 	className?: string;
@@ -35,89 +36,67 @@ const Input: React.FC<Props> = ({
 	autoComplete,
 	dark
 }) => {
+	let content: React.ReactNode = null;
+
 	switch (type) {
 		case 'textarea':
-			return (
-				<div className={[classes.Main, className].join(' ')}>
-					{label && (
-						<label className={classes.Label} htmlFor={name}>
-							{label}
-							{info && (
-								<span className={classes.Info}>
-									<HelpOutline />
-
-									<Tooltip className={classes.Tooltip} dark={dark}>
-										{info}
-									</Tooltip>
-								</span>
-							)}
-						</label>
-					)}
-					<div
-						className={[classes.Wrapper, dark ? classes.Dark : null, error ? classes.Error : null].join(
-							' '
-						)}>
-						{prefix && <span className={classes.Prefix}>{prefix}</span>}
-						<textarea
-							name={name}
-							className={[
-								classes.Textarea,
-								dark ? classes.Dark : null,
-								error ? classes.Error : null
-							].join(' ')}
-							style={label || prefix ? { width: '100%', padding: 0 } : {}}
-							placeholder={placeholder}
-							onChange={(e) => onChange(e.target.value)}
-							onFocus={onFocus}
-							onBlur={onBlur}
-							value={value}
-							disabled={disabled}
-						/>
-					</div>
-					{error && <small className={classes.Message}>{error}</small>}
-				</div>
+			content = (
+				<textarea
+					name={name}
+					className={[classes.Textarea, dark ? classes.Dark : null, error ? classes.Error : null].join(' ')}
+					style={label || prefix ? { width: '100%', padding: 0 } : {}}
+					placeholder={placeholder}
+					onChange={(e) => onChange(e.target.value)}
+					onFocus={onFocus}
+					onBlur={onBlur}
+					value={value}
+					disabled={disabled}
+				/>
 			);
+			break;
 
 		default:
-			return (
-				<div className={[classes.Main, className].join(' ')}>
-					{label && (
-						<label className={classes.Label} htmlFor={name}>
-							{label}
-							{info && (
-								<span className={classes.Info}>
-									<HelpOutline />
-
-									<Tooltip className={classes.Tooltip} dark={dark}>
-										{info}
-									</Tooltip>
-								</span>
-							)}
-						</label>
-					)}
-					<div
-						className={[classes.Wrapper, dark ? classes.Dark : null, error ? classes.Error : null].join(
-							' '
-						)}>
-						{prefix && <span className={classes.Prefix}>{prefix}</span>}
-						<input
-							name={name}
-							className={classes.Input}
-							style={label || prefix ? { width: '100%', padding: 0 } : {}}
-							placeholder={placeholder}
-							type={type}
-							onChange={(e) => onChange(e.target.value)}
-							onFocus={onFocus}
-							onBlur={onBlur}
-							value={value}
-							disabled={disabled}
-							autoComplete={autoComplete ? 'on' : 'off'}
-						/>
-					</div>
-					{error && <small className={classes.Message}>{error}</small>}
-				</div>
+			content = (
+				<input
+					name={name}
+					className={classes.Input}
+					style={label || prefix ? { width: '100%', padding: 0 } : {}}
+					placeholder={placeholder}
+					type={type}
+					onChange={(e) => onChange(e.target.value)}
+					onFocus={onFocus}
+					onBlur={onBlur}
+					value={value}
+					disabled={disabled}
+					autoComplete={autoComplete ? 'on' : 'off'}
+				/>
 			);
+			break;
 	}
+
+	return (
+		<div className={[classes.Main, className].join(' ')}>
+			{label && (
+				<label className={classes.Label} htmlFor={name}>
+					{label}
+					{info && (
+						<span className={classes.Info}>
+							<HelpOutline />
+
+							<Tooltip className={classes.Tooltip} dark={dark}>
+								{info}
+							</Tooltip>
+						</span>
+					)}
+				</label>
+			)}
+			<div className={[classes.Wrapper, dark ? classes.Dark : null, error ? classes.Error : null].join(' ')}>
+				{prefix && <span className={classes.Prefix}>{prefix}</span>}
+				{content}
+			</div>
+			{error && <small className={classes.Message}>{error}</small>}
+		</div>
+	);
 };
 
 export default Input;
