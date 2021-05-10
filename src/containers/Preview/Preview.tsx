ac098da3511e2e40fd53a9fe246302c7ec26f02f
MiVladie <justvladon@outlook.com>
2021-05-09 21:37:17 -0500

    ✊ minor performance tweaks

--- src/containers/Preview/Preview.tsx
+++ src/containers/Preview/Preview.tsx
@@ -2,36 +2,49 @@ import React, { useContext, useEffect, useState } from 'react';
 
 import { WebsiteContext } from 'context/providers/website';
 import { ActivityContext } from 'context/providers/activity';
+import { getComponentById } from 'library';
+
+import Loader from 'containers/Loader/Loader';
 
 import classes from './Preview.module.scss';
-import { getComponentById } from 'library';
 
 const Preview: React.FC = () => {
 	const [active, setActive] = useState<number>();
+
+	const [content, setContent] = useState<JSX.Element[]>();
+	const [loading, setLoading] = useState<boolean>(true);
 
 	const { state: stateWebsite } = useContext(WebsiteContext);
 	const { state: stateActivity } = useContext(ActivityContext);
 
 	useEffect(() => {
-		const homePage = stateWebsite!.pages[0].id;
+		if ((active != null && stateActivity.activePage == null) || stateActivity.activePage === active) {
+			return;
+		}
 
-		setActive(homePage);
-	}, []);
+		const activePage = stateActivity.activePage || stateWebsite!.pages[0].id;
 
-	useEffect(() => {
-		const activePage = stateActivity.activePage;
-
-		if (activePage != null) {
+		if (activePage != null && active !== activePage) {
 			setActive(activePage);
+
+			renderComponents(activePage, true);
 		}
 	}, [stateActivity.activePage]);
 
-	const renderComponents = () => {
+	useEffect(() => {
 		if (active == null) {
 			return;
 		}
 
-		return stateWebsite!.pages
+		renderComponents(active);
+	}, [stateWebsite?.pages]);
+
+	const renderComponents = async (active: number, load?: boolean) => {
+		if (load) {
+			setLoading(true);
+		}
+
+		const newContent = stateWebsite!.pages
 			.find((page) => page.id === active)!
 			.components.map((component) => {
 				const bulkComponent = getComponentById(component.componentId);
@@ -41,9 +54,15 @@ const Preview: React.FC = () => {
 
 				return <bulkComponent.default {...content} styles={styles.properties} key={component.id} />;
 			});
+
+		setContent(newContent);
+
+		if (load) {
+			setLoading(false);
+		}
 	};
 
-	return <div className={classes.Preview}>{renderComponents()}</div>;
+	return <div className={classes.Preview}>{loading ? <Loader /> : content}</div>;
 };
 
 export default Preview;
